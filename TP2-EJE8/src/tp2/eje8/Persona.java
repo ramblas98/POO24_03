80c4e5b52edc7de94f99debd0029e3262896a676
usuario <usuario@DESKTOP-OES5E7K>
2024-04-15 16:31:57 -0300

    Version 2.0 tp2 eje8

---
+++ TP2-EJE8/src/tp2/eje8/Persona.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2.eje8;
+
+/**
+ *
+ * @author usuario
+ */
+public class Persona {
+    private String nombre;
+    private String apellido;
+    private int dni;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, String apellido, int dni) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "nombre=" + nombre + ", apellido=" + apellido + ", dni=" + dni + '}';
+    }
+    
+    
+}
